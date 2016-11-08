61f3b847e440e51271e69c66bccd6b102c2b0620
tabyouto <tabyouto@gmail.com>
2016-11-08 17:22:36 +0800

    form

--- xpic/src/views/login.vue
+++ xpic/src/views/login.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <form class="form">
-
-      <button type="button" class="btn btn-default">登录</button>
-    </form>
+    <div class="col-md-5">
+      <form class="form">
+        <h3 class="text-center">Login</h3>
+        <div class="form-group">
+          <input id="email" class="form-control" placeholder="email" type="email">
+          <br>
+          <input id="password" class="form-control" type="password">
+        </div>
+        <button type="button" class="btn btn-default">登录</button>
+      </form>
+    </div>
   </div>
 </template>
 <script>
