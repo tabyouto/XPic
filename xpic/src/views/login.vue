4d47cdf68b4d1173886aabe3899511cdc4b31710
tabyouto <tabyouto@gmail.com>
2016-11-08 17:48:33 +0800

    add

--- xpic/src/views/login.vue
+++ xpic/src/views/login.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-    <div class="col-md-5">
+  <div class="">
+    <div class="col-md-4 col-md-offset-4 form-wrap">
       <form class="form">
         <h3 class="text-center">Login</h3>
         <div class="form-group">
           <input id="email" class="form-control" placeholder="email" type="email">
           <br>
-          <input id="password" class="form-control" type="password">
+          <input id="password" class="form-control" type="password" placeholder="password">
         </div>
-        <button type="button" class="btn btn-default">登录</button>
+        <button type="button" class="btn btn-default btn-block btn-login">登录</button>
       </form>
     </div>
   </div>
